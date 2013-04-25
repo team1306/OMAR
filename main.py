786aa5387cb4e7bac5d775b571e0bb020ad1da12
James Tautges <jamesdtautges@gmail.com>
2013-04-24 23:50:27 -0500

    Changed default filename

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import csvtojson, ranking, argparse
 
 # parse the tags to get the name of the input file and the respective climbing and shooting weights
 parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--file", default="./AMCscouting2013.csv")
+parser.add_argument("-f", "--file", default="./report.csv")
 parser.add_argument("-s", "--shooter", type=float, help="shooter weighting factor", default=1)
 parser.add_argument("-c", "--climber", type=float, help="climber weighting factor", default=1)
 
@@ -15,4 +15,5 @@ r = numpy.array(ranking.rank(csvtojson.csvtojson(args.file), args.climber, args.
 s = ''
 for x in r[:,0][::-1]:
     s += str(x) + '\n'
-print s
+f = open("rankings", "w")
+f.write(s)
