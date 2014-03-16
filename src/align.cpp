0b28ec7d3a8877e0ee5afc105e0d33b9f5e2acfd
James Tautges <jamesdtautges@gmail.com>
2014-03-16 14:28:01 -0500

    Changed constants and scaling to more optimal values

--- src/align.cpp
+++ src/align.cpp
@@ -14,7 +14,7 @@ void getCalibrationCircles(const Mat& src, vector<Vec3f>& real) {
   cvtColor(source, gray, CV_BGR2GRAY);
   //GaussianBlur(gray, gray, Size(9, 9), 2, 2);
  
-  HoughCircles(gray, circles, CV_HOUGH_GRADIENT, 0.5, 20, 15, 15, 3, 20);
+  HoughCircles(gray, circles, CV_HOUGH_GRADIENT, 0.5, 20, 20, 15, 3, 20);
 
   int dista, distb, distc, distd; // a is top left, b is top right, c is bottom left, d is bottom right
   int a, b, c, d;
