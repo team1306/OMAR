2c7876194d65f7dd26d881a1f1ce1305ecd04f82
James Tautges <jamesdtautges@gmail.com>
2014-12-14 20:43:39 -0600

    Fooling around with OpenCV feautures

--- src/blobtest.cpp
+++ src/blobtest.cpp
@@ -1,41 +1,95 @@
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 #include <vector>
+#include <iostream>
 
 using namespace cv;
+using namespace std;
 
-int main() {
-  Mat image = imread("./../oldscans/1-1.jpg");
-  SimpleBlobDetector::Params params;
-  params.minDistBetweenBlobs = 50.0f;
-  params.filterByInertia = false;
-  params.filterByConvexity = false;
-  params.filterByColor = false;
-  params.filterByCircularity = false;
-  params.filterByArea = true;
-  params.minArea = 20.0f;
-  params.maxArea = 500.0f;
-  // ... any other params you don't want default value
+void help()
+{
+  cout << "\nThis program demonstrates line finding with the Hough transform.\n"
+         "Usage:\n"
+    "./houghlines <image_name>, Default is pic1.jpg\n" << endl;
+}
 
-  // set up and create the detector using the parameters
-  Ptr<FeatureDetector> blob_detector = new SimpleBlobDetector(params);
-  blob_detector->create("SimpleBlob");
+int main(int argc, char** argv)
+{
+  const char* filename = argc >= 2 ? argv[1] : "pic1.jpg";
 
-  // detect!
-  std::vector<KeyPoint> keypoints;
-  blob_detector->detect(image, keypoints);
+  Mat src = imread(filename, 0);
+  resize(src, src, Size(625, 813), 0, 0, INTER_LINEAR);
+  if(src.empty())
+    {
+      help();
+      cout << "can not open " << filename << endl;
+      return -1;
+    }
 
-  // extract the x y coordinates of the keypoints: 
+  Mat orig, dst;
+  Canny(src, dst, 50, 200, 3);
+  cvtColor(dst, orig, CV_GRAY2BGR);
 
-  Mat display = image.clone();
+  vector<Vec2f> lines;
+  HoughLines(dst, lines, 1, CV_PI/180, 100, 0, 0 );int key = 0;
+  
+  int hi = 1, lo = 1;
+  int res = 50;
 
-  std::vector<int> X, Y;
-  std::vector<float> R;
-  for (int i=0; i<keypoints.size(); i++){
-    circle(display, keypoints[i].pt, int(keypoints[i].size/2), Scalar(255, 0, 0), 3, 8, 0);
-    X.push_back(keypoints[i].pt.x); 
-    Y.push_back(keypoints[i].pt.y);
-    R.push_back(keypoints[i].size/2);
-  }
-  imshow("circles", display);
+  namedWindow("circles", 1);
+  createTrackbar("hi", "circles", &hi, 255);
+  createTrackbar("lo", "circles", &lo, 255);
+  createTrackbar("res", "circles", &res, 255);
+
+    do
+      {
+        // update display and snooker, so we can play with them
+        Mat display;
+	orig.copyTo(display);
+
+        Mat snooker;
+        cvtColor(orig, snooker, CV_RGB2GRAY);
+	//GaussianBlur(snooker, snooker, Size(9, 9), 2, 2);
+
+        vector<Vec3f> circles;
+
+        // also preventing crash with hi, lo threshold here...
+        HoughCircles(snooker, circles, CV_HOUGH_GRADIENT, res/100+0.1, snooker.rows/10, hi > 0 ? hi : 1, lo > 0 ? lo : 1, 0, snooker.rows/60);
+        for( size_t i = 0; i < circles.size(); i++ )
+	  {
+	    Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+	    int radius = cvRound(circles[i][2]);
+
+	    // draw the green circle center
+	    circle( display, center, 3, Scalar(0,255,0), -1, 8, 0 );
+
+	    // draw the blue circle outline
+	    circle( display, center, radius, Scalar(255,0,0), 3, 8, 0 );
+	  }
+
+	Mat tiny;
+	resize(display, tiny, Size(417, 542), 0, 0, INTER_LINEAR);
+        imshow("circles", tiny);
+        imshow("snooker", snooker);
+        key = waitKey(33);
+      } while((char)key != 27);
+    return 0;
+
+  /*for( size_t i = 0; i < lines.size(); i++ )
+    {
+      float rho = lines[i][0], theta = lines[i][1];
+      Point pt1, pt2;
+      double a = cos(theta), b = sin(theta);
+      double x0 = a*rho, y0 = b*rho;
+      pt1.x = cvRound(x0 + 1000*(-b));
+      pt1.y = cvRound(y0 + 1000*(a));
+      pt2.x = cvRound(x0 - 1000*(-b));
+      pt2.y = cvRound(y0 - 1000*(a));
+      line( cdst, pt1, pt2, Scalar(0,0,255), 3, CV_AA);
+      }
+  imshow("source", src);
+  imshow("detected lines", cdst);
+
+  waitKey();*/
+
 }
--- src/variables.cpp
+++ src/variables.cpp
@@ -5,30 +5,32 @@
 using namespace cv;
 
 int hi = 1, lo = 1;
+int res = 50;
 
 int main(int argc, char* argv[]) {
-  Mat orig = imread("./scans/scan1.jpg");
-  pyrDown(orig, orig, Size(orig.cols/2, orig.rows/2));
-  pyrDown(orig, orig, Size(orig.cols/2, orig.rows/2));
-  pyrDown(orig, orig, Size(orig.cols/2, orig.rows/2));
+  Mat orig = imread(argv[1]);
+  resize(orig, orig, Size(625, 813), 0, 0, INTER_LINEAR);
   int key = 0;
 
   namedWindow("circles", 1);
   createTrackbar("hi", "circles", &hi, 255);
   createTrackbar("lo", "circles", &lo, 255);
+  createTrackbar("res", "circles", &res, 255);
 
     do
       {
         // update display and snooker, so we can play with them
-        Mat display = orig.clone();
+        Mat display;
+	orig.copyTo(display);
 
         Mat snooker;
         cvtColor(orig, snooker, CV_RGB2GRAY);
+	GaussianBlur(snooker, snooker, Size(9, 9), 2, 2);
 
         vector<Vec3f> circles;
 
         // also preventing crash with hi, lo threshold here...
-        HoughCircles(snooker, circles, CV_HOUGH_GRADIENT, 0.5, 20, hi > 0 ? hi : 1, lo > 0 ? lo : 1, 3, 20);
+        HoughCircles(snooker, circles, CV_HOUGH_GRADIENT, res/100+0.1, snooker.rows/10, hi > 0 ? hi : 1, lo > 0 ? lo : 1, 0, snooker.rows/60);
         for( size_t i = 0; i < circles.size(); i++ )
 	  {
 	    Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
@@ -41,7 +43,9 @@ int main(int argc, char* argv[]) {
 	    circle( display, center, radius, Scalar(255,0,0), 3, 8, 0 );
 	  }
 
-        imshow( "circles", display );
+	Mat tiny;
+	resize(display, tiny, Size(417, 542), 0, 0, INTER_LINEAR);
+        imshow("circles", tiny);
         imshow("snooker", snooker);
         key = waitKey(33);
       } while((char)key != 27);
