27ddaf1b5bd1220a9abb618f15010613399add6e
James Tautges <jamesdtautges@gmail.com>
2015-02-08 12:44:36 -0600

    Added some doxygen comments

--- src/align.h
+++ src/align.h
@@ -1,3 +1,11 @@
+/**
+ * @file align.h
+ * @brief This file contains tools for finding the calibration circles and 
+ * cropping and rotating the image to line up with those circles. The backbone 
+ * is the Hough circle transform in getCalibrationCircles, and the rest of the 
+ * methods are meant to utilize those circles.
+ */
+
 #ifndef __ALIGN_H__
 #define __ALIGN_H__
 
@@ -9,7 +17,25 @@
 
 using namespace cv;
 
+/**
+ * Read an image and write the four strongest circles closest to the corners 
+ * to the output vector of points.
+ *
+ * @param src Source image to read
+ * @param real Output vector containing the coordinates of the calibration 
+ * circles
+ */
+
 void getCalibrationCircles(const Mat&, std::vector<Vec3f>&);
+
+/**
+ * Find the angle in radians of the line connecting the first two points in a 
+ * vector with respect to the horizon.
+ *
+ * @param circles Vector of four points, only first two of which will be used
+ * @return The angle between the points in radians
+ */
+
 double getAngleOffsetTop(std::vector<Vec3f>);
 double getAngleOffsetBottom(std::vector<Vec3f>);
 double getAngleOffset(std::vector<Vec3f>);
